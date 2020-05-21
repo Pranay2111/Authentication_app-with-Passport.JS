d20ed5048d35b7e1ed7e926a81adcbd28dac8c34
Pranay Rathor <45431359+Pranay2111@users.noreply.github.com>
2020-05-21 23:33:08 +0530

    added message

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI :'mongodb+srv://pranay123:pranay123@test-cluster-swzqf.mongodb.net/test?retryWrites=true'
+    MongoURI :'mongodb+srv://<username>:<password>@test-cluster-swzqf.mongodb.net/test?retryWrites=true'
 }
